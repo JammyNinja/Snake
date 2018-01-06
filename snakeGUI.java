ee25f9bf91b46a2a81c8b3114833a52e379c0930
Louis <jammyninja95@gmail.com>
2018-01-06 18:59:24 +0000

    9

--- snakeGUI.java
+++ snakeGUI.java
@@ -11,59 +11,71 @@ public class snakeGUI extends JPanel
 	int gameWidth, gameHeight;
 	int gridXNum, gridYNum;
 	int cellWidth, cellHeight;
+	int	bannerHeight = 20;
 
 	Color background = Color.BLACK;
 	Color foreground = Color.WHITE;
 	Color snakeColour = Color.RED;
 	Frame f;
+	Font guiFont = new Font("Comic Sans MS", Font.PLAIN, 25);
+
 
 	public snakeGUI(snakeGame game){
 		this.game = game;
 		this.snake = game.snake;
 
-		f = new Frame();
+		f = new Frame(bannerHeight);
+		//f.setPreferredSize(new Dimension(250, 250 + bannerHeight) );
 		initialiseGUI(f);
 	}
 	public void initialiseGUI (Frame f){
 		//some visual constants
 		setOpaque(true);
 		setBackground(Color.BLACK);
-		setPreferredSize(new Dimension(f.frameWidth, f.frameHeight) );
+		setPreferredSize(new Dimension(f.frameWidth, f.gameHeight) );
 
 		gameWidth = f.frameWidth;
-		gameHeight = f.frameHeight;
-		gridXNum = 25;
-		gridYNum = 25;
+		gameHeight = f.gameHeight - bannerHeight;
+		gridXNum = 10;
+		gridYNum = 10;
 		cellWidth = gameWidth / gridXNum;
-		cellHeight = gameWidth / gridYNum;
+		cellHeight = gameHeight / gridYNum;
 		
 		//get wet for input
 		addKeyListener(this);
 		setFocusable(true);
-
+		
 		f.initialise(this);
 	}
 
 	public void paint(Graphics g){
 		//g.setColor(foreground);
-		//g.setFont(new Font("Monospaced", Font.PLAIN, sqSize/2) ); //for king hell
+		g.setFont(new Font("Comic Sans MS", Font.PLAIN, 15)); //for king hell
 		paintEnvironment(g);
 		paintSnake(g);
+		paintBanner(g);
 	}
 
 	public void paintEnvironment(Graphics g){
 		g.setColor(Color.WHITE);
-		for(int i = 0; i<=gridXNum; i++){
-			g.drawLine(i*cellWidth, 0, i*cellWidth, gameHeight);
+		//draw vertical lines
+		for(int i = 0; i<=gridXNum+1; i++){
+			g.drawLine(i*cellWidth, bannerHeight, i*cellWidth, gameHeight + bannerHeight);
 		}
+		//draw right-mostvertical line, cant explain why
+		g.drawLine(gameWidth-1, bannerHeight, gameWidth-1, gameHeight + bannerHeight);
+
+		//draw horizotnal lines
 		for(int j=0; j<=gridYNum; j++){
-			g.drawLine(0,j*cellHeight, gameWidth, j*cellHeight); //x1,y1,x2,y2
+			g.drawLine(0,j*cellHeight + bannerHeight, gameWidth, j*cellHeight + bannerHeight); //x1,y1,x2,y2
 		}
+		//the bottom line, compensating for banner
+		g.drawLine(0,gameHeight + bannerHeight -3, gameWidth,gameHeight + bannerHeight -3 );
 
 		//paint food
-		if(game.foodAvailable){
+		if(game.food.available){
 			g.setColor(Color.GREEN);
-			g.fillRect(game.food.x*cellWidth, game.food.y*cellHeight, cellWidth, cellHeight);
+			g.fillRect(game.food.location.x*cellWidth, game.food.location.y*cellHeight + bannerHeight, cellWidth, cellHeight);
 		}
 	}
 
@@ -71,35 +83,57 @@ public class snakeGUI extends JPanel
 		g.setColor(snakeColour);
 
 		for (Point bodyPart : snake.body){
-			g.fillRect(bodyPart.x*cellWidth, bodyPart.y * cellHeight, cellWidth, cellHeight);//x,y,width,height
+			g.fillRect(bodyPart.x*cellWidth, bodyPart.y * cellHeight + bannerHeight, cellWidth, cellHeight);//x,y,width,height
 		}
 		//draw head
 		g.setColor(Color.CYAN);
-		g.fillRect(snake.body.get(0).x*cellWidth, snake.body.get(0).y * cellHeight, cellWidth, cellHeight);//x,y,width,height
+		g.fillRect(snake.body.get(0).x*cellWidth, snake.body.get(0).y * cellHeight + bannerHeight, cellWidth, cellHeight);//x,y,width,height
 
+	}
+	public void paintBanner(Graphics g){
+		g.setColor(Color.WHITE);
+		g.drawString("SCORE:", gameWidth - 150,15);
+		g.drawString("" + game.score, gameWidth - 50, 15);
+
+		if(game.paused == true){
+			g.drawString("PAUSED!", 50,15);
+		}
 	}
 	// INPUT
 	public void keyPressed(KeyEvent e){
 		switch(e.getKeyCode()){
-			case KeyEvent.VK_UP:
-				System.out.println("UP!");
-				snake.direction = 0;
+			case KeyEvent.VK_UP: //0
+				//System.out.println("UP!");
+				if(snake.direction != 2) {
+					snake.direction = 0;
+				}
 			break;
-			case KeyEvent.VK_DOWN:
-				System.out.println("DOWN!");
-				snake.direction = 2;
+			case KeyEvent.VK_DOWN: //2
+				//System.out.println("DOWN!");
+				if(snake.direction != 0) {
+					snake.direction = 2;
+				}
 			break;
-			case KeyEvent.VK_LEFT:
-				System.out.println("LEFT!");
-				snake.direction = 3;
+			case KeyEvent.VK_LEFT: //3
+				//System.out.println("LEFT!");
+				if(snake.direction != 1) {
+					snake.direction = 3;
+				}
 			break;
-			case KeyEvent.VK_RIGHT:
-				System.out.println("RIGHT!");
-				snake.direction = 1;
+			case KeyEvent.VK_RIGHT: //1
+				//System.out.println("RIGHT!");
+				if(snake.direction != 3) {
+					snake.direction = 1;
+				}
 			break;
 			case KeyEvent.VK_SPACE:
-				game.newGame();
+				System.out.println("SPACE!");
+				game.resetGame();
+			break;
 			//pause?
+			case KeyEvent.VK_P:
+				System.out.println("P for pause!");
+				game.pauseGame();
 			break;
 			case KeyEvent.VK_ESCAPE:
 				System.exit(0);
@@ -115,12 +149,12 @@ public class snakeGUI extends JPanel
 
 
 class Frame extends JFrame {
-	static int frameWidth = 500; //windowWidth = 1000; player 1/100 of this, ball 1/50
-	static int frameHeight = 500; //windowHeight = 600;  //player is 1/6 of this
+	static int frameWidth = 300; //windowWidth = 1000; player 1/100 of this, ball 1/50
+	static int gameHeight = 300; //windowHeight = 600;  //player is 1/6 of this
 
-	public Frame(){
+	public Frame(int bannerHeight){
 		setTitle("Snake");
-		setSize(frameWidth,frameHeight);
+		setSize(frameWidth,gameHeight + bannerHeight);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
--- snakeGame.java
+++ snakeGame.java
@@ -6,88 +6,139 @@ import java.awt.event.ActionListener;
 import java.util.concurrent.ThreadLocalRandom; //for use for food placement
 
 public class snakeGame implements ActionListener{
-/* ToDo
+/* ToDo GIT THIS MOTHERFUCKER
 current: 
+	food spawns within snake body... ouch
+	points
 	food (class)
-	snake moving into itself dead
 	game edge kill
 	snake growth
-	points
 	game over / restart
+	randomise start direction - or start with direction press
+	improve gui, remove grid, add border for points
+	make the grid smaller (perhaps also function of diffiuclty)?
+	food time adapt for snakelength
+	potentially stop snake from going back in on itself
+bugs:
+	main class food check doesnt work
+	things are printing twice?
+developments:
+	multiple foods
+	special foods
 	difficulties
+tidying:
+	print function
 */
 	static snakeGame game;
 	static snakeGUI gui;
 	static snake snake;
-	static Point food;
-	static Boolean foodAvailable;
-	static int foodCount, foodTimeLimit;
-	static int points;
+	static int score;
+	static food food;
+	static boolean paused = false;
 	Timer t;
 
 	public static void main(String args[]){
 		System.out.println("Snake!");
 		game = new snakeGame();
 		gui = new snakeGUI(game);
-		snake = new snake(gui);
+		food = new food(game,gui);
+		snake = new snake(gui,food);
+
 		newGame();
-
 	}
+	//semi redundant, organise with reset...
 	public static void newGame(){
-		snake.reset();
-		
-		foodAvailable = false;
-		foodCount = 0;
-		foodTimeLimit = 150; //unhardcode
+		game.t = new Timer(250,game); //difficulty == timer...
+		resetGame();
+		paused = false;
+	}
 
-		points = 0;
-		game.t = new Timer(250,game);
+	public static void resetGame(){
+		snake.reset();
+		food.reset();
+		score = 0;
 		game.t.start();
 	}
 
 	//called per timestep
 	public void actionPerformed(ActionEvent e) {
-		snake.move();
-		if(snake.snakeDead()){
-			gameOver();
-		}
+		if(!paused){
+			//increment food time count
+			food.countUp();
 
-		foodCount++;
-		if(foodCount / 25 >= 1){
-			newFood();
-		}
-		snake.print(foodCount);
+			//are we alive
+			if(snake.snakeDead()){
+				gameOver();
+			}
+			else {
+				//move snake
+				snake.move();
 
-		snakeEat();
+				gui.repaint();
+			}
+		}
+	}
+	public void pauseGame(){
+		paused = !paused;
 		gui.repaint();
 	}
-
-	public void newFood(){
-		foodCount = 0;
-		foodAvailable = true;
-		int foodX = ThreadLocalRandom.current().nextInt(0, gui.gridXNum);
-		int foodY = ThreadLocalRandom.current().nextInt(0, gui.gridYNum);
-
-		food = new Point(foodX,foodY);
-	}
-
-	public void snakeEat(){
-		Point head = snake.getHead();
-		//snake.print("got head: " + head);
-
-		if(head.equals(food)){
-			snake.print("NOM!");
-			//increment points
-			points += 50;
-			//destroy food
-			foodAvailable = false;
-			//reset food
-			foodCount = 0;
-		}
+	public void addPoints(int points){
+		score += points;
+		System.out.println("score: " + score);
 	}
 
 	public void gameOver(){
+		System.out.println("Game Over! Score: " + score);
 		game.t.stop();
+	}
+}
+
+class food {
+	Point location;
+	boolean available;
+	int count, countLimit; //used for timing food
+	int xLim,yLim;
+	snakeGame game;
+
+	public food(snakeGame game, snakeGUI gui){
+		this.game = game;
+		xLim = gui.gridXNum;
+		yLim = gui.gridYNum-1; //must it be -1?
+		countLimit = ((xLim + yLim) / 2);//25; //turn into a factor of gui/difficulty
+		System.out.println("food count limit " + countLimit);
+		count = countLimit - 5; //makes food appear sooner, when i sort new/resetgame
+		//reset();
+	}
+	//used when food is eaten 
+	public void reset(){
+		available = false;
+		count = 0;
+	}
+	public void countUp(){
+		//System.out.println("counting up " + count);
+		count++;
+
+		if(count % countLimit == 0){
+			placeFood();
+		}
+	}
+
+	public void eaten(){
+		reset();
+		//increment points
+		game.addPoints(500);
+	}
+	public void placeFood(){
+		count = 0;
+		int foodX = ThreadLocalRandom.current().nextInt(0, xLim);
+		int foodY = ThreadLocalRandom.current().nextInt(0, yLim);
+
+		location = new Point(foodX,foodY); //should it be new point?
+		available = true;
+	}
+
+	public Point getLoc(){
+		return location;
 	}
 }
 
@@ -96,9 +147,11 @@ class snake {
 	static ArrayList<Point> body; //size(),add(),get(i), remove()
 	static int direction; //0 north, 1 east, 2 south, 3 west //for now? is there better?
 	snakeGUI gui;
+	food food;
 
-	public snake(snakeGUI gui){
+	public snake(snakeGUI gui, food f){
 		this.gui = gui;
+		this.food = f;
 		reset();
 
 		System.out.println("snake constructed");
@@ -110,11 +163,10 @@ class snake {
 	}
 	public void move(){	
 		//print("moving snake " + body);		//use direction to move head
-		
+	//update head position
 		Point head = new Point();
 		head.x = body.get(0).x;
 		head.y = body.get(0).y;
-
 		switch(direction){
 			case 0: //north
 				head.y--;
@@ -129,21 +181,33 @@ class snake {
 				head.x--;
 			break;
 		}
+		//Point head = snake.getHead();
+		//snake.print("got head: " + head);
+
+	//now actually move the snake
+
+		//IF EATEN FOOD, DONT BOTHER WITH THE REST, 'just' add new head!
+		boolean foodEaten = false;
+		Point tail = null;
+		if(head.equals(food.getLoc()) && food.available == true){
+			print("snake reckons its eaten the food");
+			//body.add(food.getLoc());
+			food.eaten();
+			foodEaten = true;
+			tail = body.get(body.size()-1);
+		}
 
 		//body follows itself
 		for (int i=body.size()-1; i>0; i--){
 			Point next = body.get(i-1);
 			body.set(i, next);
-		}
-		
+		}	
 		//move head having used it for first body part
 		body.set(0,head);
-
-		//check is the game over
-		snakeDead();
-
-		//check if hit food
-
+		if(foodEaten) {
+			body.add(tail);
+			food.countLimit++;
+		}
 
 		//print(body + "\n");
 	}
@@ -153,30 +217,32 @@ class snake {
 		for(int i = 1; i < body.size(); i++){
 			if(body.get(i).equals(head)){
 				print("HEAD OVERLAP DEAD!");
-				return true;
+				return true; //dead
 			}
 		}
+
 		//if snake head out of bounds you DIE!
 		if(head.x < 0 || head.x >= gui.gridXNum){
 			print("out of the horizontal bounds!");
-			return true;
+			return true; //dead
 		}
 		else if(head.y < 0 || head.y >= gui.gridYNum-1){ //wish I could tell you why -1
 			print("out of the vertical bounds!");
-			return true;
+			return true; //dead
 		}
-		return false;
+
+		return false; //alive
 	}
 
 	public void reset(){
 		body = new ArrayList<Point>();
-		body.add(new Point(10,5));
-		body.add(new Point(11,5));
-		body.add(new Point(12,5));
-		body.add(new Point(13,5));
-		body.add(new Point(14,5));
+		body.add(new Point(6,1));
+		body.add(new Point(5,1));
+		body.add(new Point(4,1));
+		body.add(new Point(3,1));
+		body.add(new Point(2,1));
 		
-		direction = 3;
+		direction = 2;
 	}
 	public Point getHead(){
 		return body.get(0);
